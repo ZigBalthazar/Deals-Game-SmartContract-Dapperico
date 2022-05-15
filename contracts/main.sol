14c8b57053858f3a0c8ef122548762ffc1e3a268
MuhammadAli Alizadeh <zigbalthazar@gmail.com>
2022-05-15 15:55:18 +0430

    Time/Price set

--- contracts/main.sol
+++ contracts/main.sol
@@ -6,9 +6,9 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "./timer.sol";
+import "./RandomNumber.sol";
 
-contract DealsGame is Ownable, Pausable, Timer {
+contract DealsGame is Ownable, Pausable, Random {
     using Counters for Counters.Counter;
     using SafeMath for uint256;
     Counters.Counter private _ID;
@@ -19,27 +19,20 @@ contract DealsGame is Ownable, Pausable, Timer {
         uint256 Max_Ticket_Per_Wallet;
         uint256 Start_Time;
         uint256 End_Time;
-        uint256[5] Winner_Precentage;
+        uint256[6] Winner_Precentage;
         Status _Status;
         Payment_Methods _Payment_Methods;
-        address[] Wallets; //111111111111
+        address[] Wallets;
     }
 
     mapping(uint256 => Lottery) public Lotteries;
-    mapping(uint256 => mapping(address => uint256[])) Tickets; //1111111111111
+    mapping(uint256 => mapping(address => uint256[])) Tickets;
     mapping(uint256 => uint256) Ticket_Amounts;
     uint256[] _b;
 
-    uint256[] public aaa;
-    address[] public _2;
-    address[] public _3;
-    address[] public _4;
-    address[] public _5;
-    address[] public _6;
-
     enum Payment_Methods {
-        Ethereum,
-        Tether
+        BNB,
+        BUSD
     }
     // Represents the status of the lottery
     enum Status {
@@ -56,7 +49,7 @@ contract DealsGame is Ownable, Pausable, Timer {
         uint256 _Max_Ticket_Per_Wallet,
         uint256 _Start_Time,
         uint256 _End_Time,
-        uint256[5] memory _Winner_Precentage,
+        uint256[6] memory _Winner_Precentage,
         Status _Status,
         Payment_Methods _PM,
         address[] memory _Players
@@ -82,11 +75,11 @@ contract DealsGame is Ownable, Pausable, Timer {
     {
         require(_Lottery_Id <= _ID.current(), "");
         //payment
-        require(Ticket_Amounts[_Lottery_Id] < _Tickets_Codes.length, "");
-        require(
-            Tickets[_Lottery_Id][msg.sender].length < _Tickets_Codes.length,
-            ""
-        );
+        // require(Ticket_Amounts[_Lottery_Id] < _Tickets_Codes.length, "");
+        // require(
+        //     Tickets[_Lottery_Id][msg.sender].length < _Tickets_Codes.length,
+        //     ""
+        // );
         require(getCurrentTime() >= Lotteries[_Lottery_Id].Start_Time, "");
         if (Lotteries[_Lottery_Id].Start_Time <= getCurrentTime()) {
             if (Lotteries[_Lottery_Id]._Status == Status.NotStarted) {
@@ -110,5 +103,24 @@ contract DealsGame is Ownable, Pausable, Timer {
         Lotteries[_Lottery_Id]._Status = _S;
     }
 
-    
+    //Close Lottery//////////////////////////////////////////////////////////NNNNNEEEEEEEDDDDD EDIT LOGIC !!!!!!!!!!!!!!/////////////////////////////////////////
+    function Close_Lottery(uint256 _Lottery_Id) public onlyOwner {
+        require(Lotteries[_Lottery_Id].End_Time <= getCurrentTime(), "");
+        if (Lotteries[_Lottery_Id]._Status != Status.Closed) {
+            Lotteries[_Lottery_Id]._Status = Status.Closed;
+        }
+    }
+
+    //Price Oracle
+
+    //split pay
+
+    //set price
+    function Set_Price(uint256 _Lottery_Id, uint256 _Price) public onlyOwner {
+        Lotteries[_Lottery_Id].Price = _Price;
+    }
+
+    function getCurrentTime() public view returns (uint256) {
+        return block.timestamp;
+    }
 }
